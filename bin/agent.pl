978700a3304ff7d479312184b56071d42f45c9d6
David.Spadea <David.Spadea@cc2a237b-a22e-0410-89ac-cf5208c6bbc1>
2007-08-05 06:59:29 +0000

    Saving a few changes. Nothing noteworthy.

--- bin/agent.pl
+++ bin/agent.pl
@@ -12,6 +12,29 @@ use strict;
 use warnings;
 use lib qw(../lib);
 
-use AdvancedSchedule::Database;
+use AdvancedScheduler::Database;
 
+use threads;
+use Thread::Queue;
 
+sub HandleExit
+{
+    $EXIT_FLAG->up;
+}
+
+=head1 WorkManager
+
+Reads pending events from the database and adds them to the work queue.
+
+This does not exit until the $EXIT_FLAG semaphore is set by a SIGTERM.
+
+=cut
+
+sub WorkManager
+{
+    my $db = AdvancedScheduler::Database->connect
+        or die ("Work Manager was unable to connect to the database!");
+        
+    
+    
+}
