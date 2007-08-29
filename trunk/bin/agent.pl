77b85c54de64265dc66a353fc951d57c11ef364c
David.Spadea <David.Spadea@cc2a237b-a22e-0410-89ac-cf5208c6bbc1>
2007-08-29 05:19:48 +0000

    Fix a few errors in Database.
    A little implementation in agent.pl. Still not useful,
    but does something now.

--- trunk/bin/agent.pl
+++ trunk/bin/agent.pl
@@ -12,21 +12,36 @@ use strict;
 use warnings;
 use lib qw(../lib);
 
-use AdvancedScheduler::Database;
-
 use threads;
 use Thread::Queue;
 
+use YAML qw(freeze thaw);
+
+our $EXIT_FLAG : shared;
+our $JobQueue : shared;
+
+$JobQueue = new Thread::Queue;
+
+$EXIT_FLAG = 0;
+
+use AdvancedScheduler::Database;
+
+$SIG{CHLD} = \&HandleExit;
+
+&WorkManager;
+
 sub HandleExit
 {
-    $EXIT_FLAG->up;
+    lock ($EXIT_FLAG);
+
+    $EXIT_FLAG = 1;
 }
 
 =head1 WorkManager
 
 Reads pending events from the database and adds them to the work queue.
 
-This does not exit until the $EXIT_FLAG semaphore is set by a SIGTERM.
+This does not exit until the $EXIT_FLAG is set by a SIGTERM.
 
 =cut
 
@@ -35,6 +50,24 @@ sub WorkManager
     my $db = AdvancedScheduler::Database->connect
         or die ("Work Manager was unable to connect to the database!");
         
+    my $sql = "select * from RunSchedule where Machine = ?" ;
     
+    while (! $EXIT_FLAG )
+    {
+        my $sth = $db->prepare($sql);
+        $sth->execute('titan');
+        
+        while (my $jobdef = $sth->fetchrow_hashref)
+        {
+            {
+                lock ($JobQueue);
+                $jobdef = freeze($jobdef);
+                print "Enqueuing:\n" . $jobdef . "\n\n";
+                $JobQueue->enqueue($jobdef);
+            }
+        }
+        
+        sleep 5;
+    }
     
 }
