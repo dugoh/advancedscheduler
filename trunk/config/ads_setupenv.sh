1e48e845461c3c4cf290654314eea2a2bb440b47
David.Spadea <David.Spadea@cc2a237b-a22e-0410-89ac-cf5208c6bbc1>
2007-09-12 01:26:57 +0000

    Added dir for configurations. ads_setupenv.sh sets up environment variables, and must be run before any ADS programs will run.

---
+++ trunk/config/ads_setupenv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export ADSHOST=localhost
+export ADSUSER=ads
+export ADSPASSWD=ads
+export ADSDB=ads
+export ADSROOT=$HOME/dev/advancedscheduler
+export ADSLOG=$ADSROOT/log
+export ADSJOBLOG=$ADSLOG/jobs
+export PATH=$PATH:$ADSROOT/bin
