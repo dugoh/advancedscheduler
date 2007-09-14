a2daab3ac629e1154a47015e9cb65aaea8bb494e
David.Spadea <David.Spadea@cc2a237b-a22e-0410-89ac-cf5208c6bbc1>
2007-09-14 03:02:35 +0000

    New test cases in the jil. Several modifications to agent:   - Multithreaded (WorkManager and ExecWork are in different threads.)   - stdin/stdout/stderr files implemented with sensible defaults

--- trunk/bin/agent.pl
+++ trunk/bin/agent.pl
@@ -51,14 +51,20 @@ $SIG{TERM} = \&HandleExit;
 # how SIGCHLD/fork/exec and system() interact with threads, so
 # I'm chickening out of that for the time being.
 
-while ( ! $EXIT_FLAG )
+my $manager = threads->create(\&WorkManager);    
+my $worker = threads->create(\&ExecWork);
+
+while (1)
 {
-    &WorkManager;
-    
-    &ExecWork;
-    
-    sleep 2;
+    last if $EXIT_FLAG;
+    sleep 1;
 }
+
+$manager->join;
+$worker->join;
+
+print "MainLoop: Got EXIT_FLAG. Quitting.\n";
+exit 0;
 
 =head1 WorkManager
 
@@ -70,7 +76,7 @@ This does not exit until the $EXIT_FLAG is set by a SIGTERM.
 
 sub WorkManager
 {
-    my $db = AdvancedScheduler::Database->connect_cached
+    my $db = AdvancedScheduler::Database->connect
         or die ("Work Manager was unable to connect to the database!");
         
     my $sql =<<SQL;
@@ -85,25 +91,32 @@ SQL
 
     
     my $sth = $db->prepare($sql);
-    $sth->execute(hostname(), $agentid);
     
-    while (my $jobdef = $sth->fetchrow_hashref)
+    while (! $EXIT_FLAG)
     {
+        $sth->execute(hostname(), $agentid);
+        
+        while (my $jobdef = $sth->fetchrow_hashref)
         {
-            lock ($JobQueue);
-            my $jobid = $$jobdef{name};
-            
-            $jobdef = freeze($jobdef);
-            print scalar localtime(time) . " Enqueuing:\n" . $jobdef . "\n\n";
-            $JobQueue->enqueue($jobdef);
-            
-            $db->SetJobStatus($jobid, 'ST');
+            {
+                lock ($JobQueue);
+                my $jobid = $$jobdef{name};
+                
+                print scalar localtime(time) . " Enqueuing:" . $$jobdef{name} . "\n";
+                $jobdef = freeze($jobdef);
+                $JobQueue->enqueue($jobdef);
+                
+                $db->SetJobStatus($jobid, 'ST');
+            }
         }
+        
+        $sth->finish;
+        
+        sleep 2; # Only query DB every two seconds.
     }
     
-    $sth->finish;
-    
-    
+    print "WorkManager: Got EXIT_FLAG. Quitting.\n";
+    return 0;
 }
 
 =head1 ExecWork
@@ -117,22 +130,60 @@ will eventually need to be some sordft of parallel implementation.
 
 sub ExecWork
 {
-    my $db = AdvancedScheduler::Database->connect_cached
+    my $jobdef;
+    
+    my $db = AdvancedScheduler::Database->connect
         or die ("ExecWork was unable to connect to the database!");
         
-    while ( my $jobdef = $JobQueue->dequeue_nb )
+    while ( ! $EXIT_FLAG )
     {
-        $jobdef = thaw($jobdef);
-        print scalar localtime(time) . " Will execute: " . Dumper($jobdef);
+        {
+            lock($JobQueue);
+            $jobdef = $JobQueue->dequeue_nb;
+        }
         
-        $db->SetJobStatus($$jobdef{name}, 'RU');
-        system($$jobdef{command});
-        my $rc = ($? >> 8);
-        print "Return status = $rc\n";
+        if ($jobdef)
+        {
+            $jobdef = thaw($jobdef);
+            print scalar localtime(time) . " Will execute: " . $$jobdef{name} . "\n";
+            
+            $db->SetJobStatus($$jobdef{name}, 'RU');
+            
+            my $cmd = &CreateCmd($jobdef);
+            
+            print $$jobdef{name} . ": Command is:\n$cmd\n";
+            system($cmd);
+            
+            my $rc = ($? >> 8);
+            print "Return status = $rc\n";
+            
+            $db->SetJobStatus($$jobdef{name}, ($rc ? 'FA' : 'SU'));
+        }
         
-        $db->SetJobStatus($$jobdef{name}, ($rc ? 'FA' : 'SU'));
+        sleep 1;
     }
+    
+    print "WorkManager: Got EXIT_FLAG. Quitting.\n";
+    return 0;
 }
 
+sub CreateCmd
+{
+    my $jobdef = shift;
+    
+    my ($in, $out, $err) =
+        ($$jobdef{std_in_file}, $$jobdef{std_out_file}, $$jobdef{std_err_file});
+    
+    $in ||= '/dev/null' unless ($in);
+    $out ||= "$ENV{ADSJOBLOG}/$$jobdef{name}.log";
+    $err ||= $out;
+
+    my $cmd = $$jobdef{command};
+    $cmd .= " < $in";
+    $cmd .= " >>$out";
+    $cmd .= " 2>>$err";
+    
+    return $cmd;
+}
 
 
