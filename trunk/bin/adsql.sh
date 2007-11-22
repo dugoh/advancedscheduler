d2a62f0edc4735232ebfef5175281d7a2419faab
David.Spadea <David.Spadea@cc2a237b-a22e-0410-89ac-cf5208c6bbc1>
2007-11-22 02:23:09 +0000

    Added help and the ability to pass a query as args instead of solely through STDIN. This was annoying me.

--- trunk/bin/adsql.sh
+++ trunk/bin/adsql.sh
@@ -5,6 +5,45 @@
 #
 # This code is release for use under the terms of the GNU General Public License, version 3.
 
+if [ "$1" = "--help" ] ; then
+
+cat <<"HEREDOC"
+
+This script can be invoked as follows:
+
+ Pipe in SQL from STDIN:
+
+   cat myquery.sql | adsql
+   echo 'select CURRENT_TIMESTAMP' | adsql
+   adsql < myquery.sql
+
+ OR
+
+ Supply the query as argument(s) to the command.
+
+   adsql select JobID, StartTime, EndTime from RunRecord
+   adsql 'select * from Job'
+
+BEWARE SHELL EXPANSIONS!
+
+ Note that the second example above has the query in quotes. This is
+ because of the '*' in the query. This will get expanded by the shell,
+ and the query that adsql actually sees will contain the names of every
+ file in the current directory!
+
+ Alternatives would be:
+
+   adsql select \* from Job
+   adsql select '*' from Job
+
+ ...basically anything that keeps the shell from expanding the * before
+ passing it into adsql.
+
+HEREDOC
+
+fi
+
+sql="$*"
 
 if [ ! -z "$ADSHOST" ] ; then
 	cmd=" -h $ADSHOST"
@@ -14,4 +53,9 @@ if [ ! -z "$ADSUSER" ] ; then
 	cmd="$cmd -U $ADSUSER"
 fi
 
-psql -d $ADSDB $cmd
+if [ ! -z "$sql" ] ; then
+    echo "$sql" | psql -d $ADSDB $cmd
+else
+    psql -d $ADSDB $cmd
+fi
+
